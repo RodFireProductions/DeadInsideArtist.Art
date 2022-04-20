c6c9ea067b18adfa5ad5adf8db6e34211d7e3c3d
RodFireProductions <53588604+RodFireProductions@users.noreply.github.com>
2022-04-20 11:46:43 -0500

    Revert "Let's see how project pages work"
    This reverts commit 466d38492d2976b36858eb5a789c8d6ff6ae90f0.

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,7 +1,6 @@
 <script>
 	import Header from '$lib/header/Header.svelte';
 	import '../app.css';
-	import './usual.css';
 	import './console.js';
 	let year = 2022;
 </script>
@@ -19,4 +18,8 @@
 </footer>
 
 <style>
+footer a {
+	color: lightblue;
+}
+
 </style>
