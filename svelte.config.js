f38d264a98454335fdf93e88d1eaba9c245ca457
RodFireProductions <53588604+RodFireProductions@users.noreply.github.com>
2023-01-03 06:58:50 -0600

    kill me

--- svelte.config.js
+++ svelte.config.js
@@ -1,18 +1,24 @@
-import adapter from '@sveltejs/adapter-auto';
+//import vercel from '@sveltejs/adapter-vercel';
+import adapt from '@sveltejs/adapter-auto';
 
-/** @type {import('@sveltejs/kit').Config} */
-const config = {
-	kit: {
-		adapter: adapter(),
+export default {
+  kit: {
+    // default options are shown
+    //adapter: vercel({
+      // if true, will deploy the app using edge functions
+      // (https://vercel.com/docs/concepts/functions/edge-functions)
+      // rather than serverless functions
+      //edge: false,
 
-		// Override http methods in the Todo forms
-		methodOverride: {
-			allowed: ['PATCH', 'DELETE']
-		}
-	},
-	experimental: {
-      useVitePreprocess: true
-    }
-};
+      // an array of dependencies that esbuild should treat
+      // as external when bundling functions
+     // external: [],
 
-export default config;
+      // if true, will split your app into multiple functions
+      // instead of creating a single one for the entire app
+      //split: false
+
+    //})
+    adapter: adapt({})
+  }
+}
